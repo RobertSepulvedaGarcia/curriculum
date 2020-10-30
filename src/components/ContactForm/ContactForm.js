274eb9dbf1a0f86bae1123edf69ac49c8d8d1e22
Robert <robert.sepulveda1995@gmail.com>
2020-10-30 17:50:30 -0300

    se crea formulario de prueba

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Container, Form, Button } from "react-bootstrap"
+
+export default function ContactForm() {
+  const { Group, Label, Control } = Form
+  return (
+    <Container>
+      <Form
+        name="contact-form"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <Group>
+          <Label>Su Nombre:</Label>
+          <Control type="text" />
+        </Group>
+        <Button variant="primary" type="submit">
+          Enviar
+        </Button>
+      </Form>
+    </Container>
+  )
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,11 @@
 import React from "react"
 import LayoutBasic from "../layouts/LayoutBasic"
+import ContactForm from "../components/ContactForm/ContactForm"
+
 export default function contact() {
   return (
     <LayoutBasic menuColor="#000">
-      <h2>como contactarme</h2>
+      <ContactForm />
     </LayoutBasic>
   )
 }
