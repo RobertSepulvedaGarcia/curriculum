a35d78dbab8847d6c02913105b75843ff8f4d9f3
Robert <robert.sepulveda1995@gmail.com>
2020-10-29 21:38:35 -0300

    commit de prueba

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import LayoutBasic from "../layouts/LayoutBasic"
 import Info from "../components/Info/Info"
 import SobreMi from "../components/SobreMi/SobreMi"
 import "./Index.scss"
+
 export default function index() {
   return (
     <LayoutBasic menuColor="rgba(102,205,170,0.2)">
