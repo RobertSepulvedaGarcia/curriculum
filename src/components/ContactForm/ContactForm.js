654a360304e20ebbf3d83a5bdad204260477098b
Robert <robert.sepulveda1995@gmail.com>
2020-10-30 19:20:49 -0300

    ahora espero que sea ultima prueba

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -9,11 +9,29 @@ export default function ContactForm() {
         name="contact-form"
         method="post"
         data-netlify="true"
+        data-netlify-honeypot="bot-field"
         onSubmit="submit"
       >
+        <input type="hidden" name="form-name" value="contact-form" />
+        <p hidden>
+          NO LLENES ESTO!:
+          <input name="bot-field" />
+        </p>
         <Group>
-          <Label>Su Nombre:</Label>
-          <Control type="text" name="name" />
+          <Label>Nombre:</Label>
+          <Control required type="text" name="nombre" />
+        </Group>
+        <Group>
+          <Label>Apellido:</Label>
+          <Control required type="text" name="apellido" />
+        </Group>
+        <Group>
+          <Label>E-mail:</Label>
+          <Control required type="email" name="email" />
+        </Group>
+        <Group>
+          <Label>¿En qué le puedo ayudar?:</Label>
+          <Control required as="textarea" rows="4" name="info" />
         </Group>
         <Button variant="primary" type="submit">
           Enviar
