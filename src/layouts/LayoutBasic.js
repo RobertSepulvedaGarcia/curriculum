a9bf72673273e02134fba6c1a51863b04b9d9e80
Robert <robert.sepulveda1995@gmail.com>
2020-10-30 01:06:42 -0300

    probando deploy

--- src/layouts/LayoutBasic.js
+++ src/layouts/LayoutBasic.js
@@ -2,10 +2,11 @@ import React from "react"
 import Menu from "../components/Menu/Menu"
 export default function LayoutBasic(props) {
   const { children, menuColor } = props
-  return (
+
+  return typeof window !== `undefined` ? (
     <>
       <Menu bgColor={menuColor} />
       {children}
     </>
-  )
+  ) : null
 }
