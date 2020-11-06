08b92ae5b16e7ee111ff079473af46db6a1e478d
Robert <robert.sepulveda1995@gmail.com>
2020-11-05 22:26:46 -0300

    deploy

--- src/layouts/LayoutBasic.js
+++ src/layouts/LayoutBasic.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Menu from "../components/Menu/Menu"
 import "./LayoutBasic.scss"
+
 export default function LayoutBasic(props) {
   const { children, menuColor } = props
 
