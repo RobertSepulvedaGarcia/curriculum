d6b2ac9b27219bcfb8ac3cc26f3bab318c855734
Robert <robert.sepulveda1995@gmail.com>
2020-11-06 16:21:13 -0300

    se agregan metadatos para mejorar el seo de la pagina

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mi curriculum/Portafolio personal`,
+    description: `Mi primer proyecto creado en gatsby para poder entregar un curriculum más acorde`,
+    author: `@RobertSepulveda`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -1,11 +1,77 @@
 import React from "react"
 import Helmet from "react-helmet"
-export default function Seo({ titulo }) {
-  return (
-    <div>
-      <Helmet>
-        <title>{titulo}</title>
-      </Helmet>
-    </div>
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+export default function Seo({ titulo, description, meta, lang }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
   )
+
+  const metaDescription = description || data.site.siteMetadata.description
+  return (
+    <>
+      <Helmet
+        htmlAttributes={{ lang }}
+        title={titulo}
+        titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+        meta={[
+          {
+            name: "Description",
+            content: metaDescription,
+          },
+          {
+            name: "og:title",
+            content: titulo,
+          },
+          {
+            name: "og:description",
+            content: metaDescription,
+          },
+          {
+            name: "og:type",
+            content: "website",
+          },
+          {
+            name: "twitter:card",
+            content: "summary",
+          },
+          {
+            name: "twitter:creator",
+            content: data.site.siteMetadata.author,
+          },
+          {
+            name: "twitter:title",
+            content: titulo,
+          },
+          {
+            name: "twitter:description",
+            content: metaDescription,
+          },
+        ].concat(meta)}
+      />
+    </>
+  )
+}
+
+Seo.defaultProps = {
+  lang: "es",
+  meta: [],
+  description: "",
+}
+
+Seo.propTypes = {
+  titulo: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
 }
